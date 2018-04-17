669fc05f1ac411e19e7c299ae7b4951ec81e333d
David <thuwdw@163.com>
2018-04-17 23:49:51 +0800

    add toDict

--- www/config.py
+++ www/config.py
@@ -10,13 +10,15 @@ class Dict(dict):
     '''
     simple dict but support access as x.y style
     '''
-    def __init__(self,**kw):
+    def __init__(self,names=(), values=(), **kw):
         super(Dict,self).__init__(**kw)
+        for k, v in zip(names,values):
+            self[k] = v
     def __getattr__(self,key):
         try:
             return self[key]
         except KeyError:
-            raise
+            raise AttributeError(r"'Dict' object has no attribute %s " % key)
     def __setattr__(self,k,v):
             self[k] = v
 
@@ -38,4 +40,10 @@ try:
 except ImportError:
     pass
 
-configs = Dict(**configs)
+def toDict(d):
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v,dict) else v
+    return D
+
+configs = toDict(configs)
